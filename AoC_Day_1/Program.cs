dc47d46bdbab283cb936cfc96d1eb6f165f9f0b9
Aaron Greiner <aarongreiner@icloud.com>
2022-12-01 17:36:43 +0100

    init day 1

---
+++ AoC_Day_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC_Day_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(@"../../input.txt");
+
+            foreach (string s in input)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
