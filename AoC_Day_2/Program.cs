fca5fbe4f882581339f11966bf1e2846c33187d5
Aaron Greiner <aarongreiner@icloud.com>
2022-12-02 21:29:23 +0100

    init day 2

---
+++ AoC_Day_2/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO;
+
+namespace AoC_Day_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(@"../../input.txt");
+            int total_score = 0;
+            int total_score_bonus = 0;
+
+            foreach (string s in input)
+            {
+                total_score += GetScore(s.ToCharArray()[0], s.ToCharArray()[2]);
+                total_score_bonus += GetScore(s.ToCharArray()[0], GetShape(s.ToCharArray()[0], s.ToCharArray()[2]));
+            }
+
+            Console.WriteLine("My Strategy Score: " + total_score);
+            Console.WriteLine("Elfs Strategy Score: " + total_score_bonus);
+
+            Console.ReadLine();
+        }
+
+        private static int GetScore(char c1, char c2)
+        {
+            switch (c1) {
+                case 'A': //Rock
+                    switch (c2)
+                    {
+                        case 'X': //Rock
+                            return 1 + 3;
+                        case 'Y': //Paper
+                            return 2 + 6;
+                        case 'Z': //Scissors
+                            return 3 + 0;
+                    }
+                    break;
+                case 'B': //Paper
+                    switch (c2)
+                    {
+                        case 'X': //Rock
+                            return 1 + 0;
+                        case 'Y': //Paper
+                            return 2 + 3;
+                        case 'Z': //Scissors
+                            return 3 + 6;
+                    }
+                    break;
+                case 'C': //Scissors
+                    switch (c2)
+                    {
+                        case 'X': //Rock
+                            return 1 + 6;
+                        case 'Y': //Paper
+                            return 2 + 0;
+                        case 'Z': //Scissors
+                            return 3 + 3;
+                    }
+                    break;
+            }
+
+            return 0;
+        }
+
+        private static char GetShape(char c1, char c2)
+        {
+            switch (c1)
+            {
+                case 'A': //Rock
+                    switch (c2)
+                    {
+                        case 'X': //Lose
+                            return 'Z';
+                        case 'Y': //Draw
+                            return 'X';
+                        case 'Z': //Win
+                            return 'Y';
+                    }
+                    break;
+                case 'B': //Paper
+                    switch (c2)
+                    {
+                        case 'X': //Lose
+                            return 'X';
+                        case 'Y': //Draw
+                            return 'Y';
+                        case 'Z': //Win
+                            return 'Z';
+                    }
+                    break;
+                case 'C': //Scissors
+                    switch (c2)
+                    {
+                        case 'X': //Lose
+                            return 'Y';
+                        case 'Y': //Draw
+                            return 'Z';
+                        case 'Z': //Win
+                            return 'X';
+                    }
+                    break;
+            }
+
+            return '-';
+        }
+    }
+}
--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 >Calories carried by Top 1: **68467**<br>
 >Calories carried by Top 3: **203420** <sup>*Bonus</sup>
 ***
+###### Output Day 2
+>My Strategy Score: **10404**<br>
+>Elfs Strategy Score: **10334** <sup>*Bonus</sup>
+***
